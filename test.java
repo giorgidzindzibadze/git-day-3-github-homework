5b0ebd6436164617941f287218f85e9d074872f6
giorgidzindzibadze <giorgi.dzindzibadze.1@btu.edu.ge>
2023-07-19 13:55:14 +0400

    branch2 commit

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is branch2, less important than branch1");
     }
 }
